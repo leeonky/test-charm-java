0dee93b9828af8f98999f903eb5f4d36eb5d3206
Liangchen <leeonky@gmail.com>
2022-01-29 21:24:06 +0800

    Update deps

--- src/test/java/com/github/leeonky/cucumber/Steps.java
+++ src/test/java/com/github/leeonky/cucumber/Steps.java
@@ -1,7 +1,5 @@
 package com.github.leeonky.cucumber;
 
-import com.github.dreamhead.moco.RequestHit;
-import com.github.dreamhead.moco.Runner;
 import com.github.leeonky.cucumber.restful.CustomPicoFactory;
 import com.github.leeonky.cucumber.restful.RestfulStep;
 import io.cucumber.java.After;
@@ -9,21 +7,21 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.verify.VerificationTimes;
 
 import java.net.URL;
 
-import static com.github.dreamhead.moco.Moco.*;
-import static com.github.dreamhead.moco.MocoRequestHit.once;
-import static com.github.dreamhead.moco.MocoRequestHit.requestHit;
-import static com.github.dreamhead.moco.Runner.runner;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.model.HttpRequest.request;
 
 public class Steps {
     private final RestfulStep restfulStep;
 
-    private Runner runner = null;
-    private RequestHit requestHit;
     private String requestedBaseUrl;
+
+    private ClientAndServer mockServer;
 
     public Steps(RestfulStep restfulStep) {
         this.restfulStep = restfulStep;
@@ -31,26 +29,29 @@ public class Steps {
 
     @Given("base url {string}")
     public void base_url(String baseUrl) {
-        CustomPicoFactory.lookupAction = s -> startMoco(s, baseUrl);
+        CustomPicoFactory.lookupAction = s -> startMockServer(s, baseUrl);
         restfulStep.setBaseUrl(baseUrl);
     }
 
     @SneakyThrows
-    private void startMoco(@NotNull String s, String baseUrl) {
+    private void startMockServer(@NotNull String s, String baseUrl) {
         assertThat(new URL(baseUrl).getHost()).isEqualTo(s);
         requestedBaseUrl = baseUrl;
-        (runner = runner(httpServer(80, requestHit = requestHit()))).start();
+        mockServer = startClientAndServer(80);
     }
 
     @Then("{string} got a GET request on {string}")
     public void got_a_get_request_on(String url, String path) {
-        requestHit.verify(and(by(uri(path)), by(method("GET"))), once());
-        assertThat(url).isEqualTo(requestedBaseUrl);
+        mockServer.verify(request()
+                        .withMethod("GET")
+                        .withPath(path),
+                VerificationTimes.once());
+        assertThat(url).as(String.format("Expect %s to receive the request, but send to %s", url, requestedBaseUrl)).isEqualTo(requestedBaseUrl);
     }
 
     @After
-    void stopMoco() {
-        if (runner != null)
-            runner.stop();
+    void stopMockServer() {
+        if (mockServer != null)
+            mockServer.stop();
     }
 }
