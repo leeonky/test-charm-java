186b3b6453b44d6a14ba3b033d1c2919cee900e5
leeonky <leeonky@gmail.com>
2022-09-21 02:42:58 +0000

    Add more function interface

--- src/main/java/com/github/leeonky/util/function/Extension.java
+++ src/main/java/com/github/leeonky/util/function/Extension.java
@@ -1,7 +1,13 @@
 package com.github.leeonky.util.function;
 
 import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import static java.util.Optional.empty;
+import static java.util.stream.Stream.of;
 
 public class Extension {
 
@@ -9,5 +15,17 @@ public class Extension {
     public static <T> Predicate<T> not(Predicate<? extends T> t) {
         Objects.requireNonNull(t);
         return (Predicate<T>) t.negate();
+    }
+
+    @SuppressWarnings("unchecked")
+    @SafeVarargs
+    public static <T> Optional<T> oneOf(Supplier<Optional<? extends T>>... optionals) {
+        return (Optional<T>) of(optionals).map(Supplier::get).filter(Optional::isPresent).findFirst().orElse(empty());
+    }
+
+    public static <T> BinaryOperator<T> notAllowParallelReduce() {
+        return (o1, o2) -> {
+            throw new IllegalStateException("Not allow parallel here!");
+        };
     }
 }
--- src/test/java/com/github/leeonky/util/function/ExtensionTest.java
+++ src/test/java/com/github/leeonky/util/function/ExtensionTest.java
@@ -3,21 +3,59 @@ package com.github.leeonky.util.function;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
 import java.util.function.Predicate;
 
+import static com.github.leeonky.util.function.Extension.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class ExtensionTest {
-
     @Nested
     class Not {
 
         @Test
-        void not_predicate() {
-            Predicate<String> predicate = s -> s.equals("true");
+        void not_() {
+            Predicate<String> predicate = str -> {
+                assertThat(str).isEqualTo("given");
+                return true;
+            };
 
-            assertThat(Extension.not(predicate).test("true")).isFalse();
-            assertThat(Extension.not(predicate).test("not true")).isTrue();
+            assertThat(not(predicate).test("given")).isFalse();
+        }
+    }
+
+    @Nested
+    class NotAllowParallelReduce {
+
+        @Test
+        void should_raise_exception() {
+            assertThrows(IllegalStateException.class, () -> notAllowParallelReduce().apply(any(), any()));
+        }
+    }
+
+    @Nested
+    class OneOf {
+
+        @Test
+        void return_empty_when_all_supplier_empty() {
+            assertThat(oneOf(Optional::empty)).isEmpty();
+            assertThat(oneOf(Optional::empty, Optional::empty)).isEmpty();
+        }
+
+        @Test
+        void return_option_value_when_present() {
+            assertThat(oneOf(Optional::empty, () -> Optional.of("hello"))).hasValue("hello");
+        }
+
+        @Test
+        void return_first_option_value_and_ignore_others() {
+            assertThat(oneOf(Optional::empty, () -> Optional.of("hello"), () -> {
+                fail();
+                return Optional.of("any str");
+            })).hasValue("hello");
         }
     }
 }
