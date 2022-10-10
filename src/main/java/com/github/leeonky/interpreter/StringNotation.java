0af7a4e6f06ca25dfabe1ae19ba434a877e12559
Liangchen <leeonky@gmail.com>
2022-10-10 22:41:35 +0800

    Reimplement string notation

--- src/main/java/com/github/leeonky/interpreter/StringNotation.java
+++ src/main/java/com/github/leeonky/interpreter/StringNotation.java
@@ -2,74 +2,120 @@ package com.github.leeonky.interpreter;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
+import java.util.stream.Collectors;
 
 public class StringNotation {
     private final String content;
     private final List<Integer> positions = new ArrayList<>();
     private final List<Integer> rows = new ArrayList<>();
+    private final List<Integer> columns = new ArrayList<>();
 
     public StringNotation(String content) {
         this.content = content;
     }
 
     public StringNotation position(int position) {
-        if (position >= 0 && position < content.length())
+        if (position >= 0 && position <= content.length())
             positions.add(position);
         return this;
     }
 
-    public String result() {
-        int startPosition = -1;
-        String newLine = "\n";
-        String content = this.content;
-        StringBuilder result = new StringBuilder();
-        while (true) {
-            String[] lines = content.split("\r\n|\n\r|\n|\r", 2);
-            result.append(lines[0])
-                    .append(markLine(startPosition, startPosition + lines[0].length(), newLine))
-                    .append(markRow(startPosition, startPosition + lines[0].length(), newLine))
-            ;
-            if (lines.length > 1) {
-                newLine = content.substring(lines[0].length(), content.length() - lines[1].length());
-                startPosition += lines[0].length() + newLine.length();
-                content = lines[1];
-                result.append(newLine);
-            } else
-                return result.toString();
-        }
-    }
-
-    private String markRow(int startPosition, int endPosition, String newLine) {
-        List<Integer> rowPositions = rows.stream()
-                .filter(i -> i <= endPosition && i > startPosition).collect(toList());
-        if (rowPositions.isEmpty())
-            return "";
-        StringBuilder builder = new StringBuilder().append(newLine);
-        for (int i = startPosition; i < endPosition; i++) {
-            builder.append('^');
-        }
-        return builder.toString();
-    }
-
-    private String markLine(int startPosition, int endPosition, String newLine) {
-        List<Integer> linePositions = positions.stream()
-                .filter(i -> i <= endPosition && i > startPosition).collect(toList());
-        if (linePositions.isEmpty())
-            return "";
-        StringBuilder builder = new StringBuilder().append(newLine);
-        int position = startPosition;
-        for (Integer linePosition : linePositions) {
-            builder.append(String.format("%" + (linePosition - position) + "c", '^'));
-            position = linePosition;
-        }
-        return builder.toString();
-    }
-
     public StringNotation row(int position) {
-        if (position >= 0 && position < content.length())
+        if (position >= 0 && position <= content.length())
             rows.add(position);
         return this;
+    }
+
+    public String result() {
+        StringBuilder result = new StringBuilder();
+        SeparatedString separatedString = new SeparatedString(content, 0, 0);
+        while (separatedString.printLine(result).hasNextLine()) {
+            separatedString = separatedString.separatedNext().newLine(result);
+        }
+        return result.toString();
+    }
+
+    public String result(int offset) {
+        StringBuilder result = new StringBuilder();
+        SeparatedString separatedString = new SeparatedString(content.substring(offset), offset, offset);
+        while (separatedString.printLine(result).hasNextLine()) {
+            separatedString = separatedString.separatedNext().newLine(result);
+        }
+        return result.toString();
+    }
+
+    public StringNotation column(int position) {
+        if (position >= 0 && position <= content.length())
+            columns.add(position);
+        return this;
+    }
+   
+//    private static boolean isFullWidth(int c) {
+//        return !('\u0000' <= c && c <= '\u00FF' || '\uFF61' <= c && c <= '\uFFDC' || '\uFFE8' <= c && c <= '\uFFEE');
+//    }
+
+    public class SeparatedString {
+        private final int startPosition;
+        private final int offset;
+        private final String newLine;
+        private final String[] lines;
+
+        public SeparatedString(String content, int startPosition, int offset) {
+            lines = content.split("\r\n|\n\r|\n|\r", 2);
+            newLine = fetchNewLine(content);
+            this.startPosition = startPosition;
+            this.offset = offset;
+        }
+
+        private String fetchNewLine(String content) {
+            return hasNextLine() ? content.substring(lines[0].length(), content.length() - lines[1].length()) : "\n";
+        }
+
+        public boolean hasNextLine() {
+            return lines.length == 2;
+        }
+
+        private SeparatedString printLine(StringBuilder builder) {
+            builder.append(lines[0]);
+            printPositions(builder, positions);
+            printPositions(builder, columns);
+            printWholeLine(builder);
+            return this;
+        }
+
+        private void printWholeLine(StringBuilder builder) {
+            List<Integer> linePositions = linePosition(rows);
+            if (!linePositions.isEmpty()) {
+                builder.append(newLine);
+                for (int i = 0; i <= lines[0].length(); i++)
+                    builder.append('^');
+            }
+        }
+
+        private void printPositions(StringBuilder builder, List<Integer> positions) {
+            List<Integer> linePositions = linePosition(positions);
+            if (!linePositions.isEmpty()) {
+                int startEndIndex = startPosition - 1;
+                builder.append(newLine);
+                for (int position : linePositions) {
+                    builder.append(String.format("%" + (position - startEndIndex) + "c", '^'));
+                    startEndIndex = position;
+                }
+            }
+        }
+
+        private List<Integer> linePosition(List<Integer> positions) {
+            return positions.stream().filter(i -> i >= startPosition && i <= startPosition + lines[0].length())
+                    .sorted().collect(Collectors.toList());
+        }
+
+        private SeparatedString separatedNext() {
+            return new SeparatedString(lines[1], startPosition + lines[0].length() + newLine.length(), offset);
+        }
+
+        private SeparatedString newLine(StringBuilder result) {
+            result.append(newLine);
+            return this;
+        }
     }
 }
