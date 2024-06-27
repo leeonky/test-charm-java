1b6986ddd1835c69081357542b77fc75ab7f8037
Liangchen <leeonky@gmail.com>
2024-06-28 00:02:15 +0800

    Fix restful-cucumber test

--- RESTful-cucumber/src/test/java/com/github/leeonky/cucumber/RunCucumberModuleSafe.java
+++ RESTful-cucumber/src/test/java/com/github/leeonky/cucumber/RunCucumberModuleSafe.java
@@ -5,11 +5,10 @@ import org.junit.jupiter.api.Test;
 import static io.cucumber.core.cli.Main.run;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RunCucumber {
-
+public class RunCucumberModuleSafe {
     @Test
     void run_cucumber() {
         assertThat(run("--plugin", "pretty", "--glue", "com.github.leeonky",
-                "src/test/resources/features")).isEqualTo(Byte.valueOf("0"));
+                "src/test/resources/features/module-safe")).isEqualTo(Byte.valueOf("0"));
     }
 }
