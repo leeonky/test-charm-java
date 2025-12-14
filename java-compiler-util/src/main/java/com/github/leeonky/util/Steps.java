c09bbe9cb112098be744793ac28eae5d58074d01
Liangchen <leeonky@gmail.com>
2025-12-14 23:30:17 +0800

    Refactor test

--- java-compiler-util/src/main/java/com/github/leeonky/util/Steps.java
+++ java-compiler-util/src/main/java/com/github/leeonky/util/Steps.java
@@ -4,7 +4,10 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import static com.github.leeonky.dal.Assertions.expectRun;
 
 public class Steps {
 
@@ -39,5 +42,16 @@ public class Steps {
     public void importDependency(Scenario scenario) {
         scenario.getSourceTagNames().stream().filter(s -> s.startsWith("@import"))
                 .forEach(s -> JavaExecutor.executor().main().importDependency(s.replace("@import(", "").replace(")", "")));
+    }
+
+    @Then("the result should be:")
+    public void the_result_should_be(String expression) {
+        expectRun(JavaExecutor.executor().main()::evaluate).should(expression);
+    }
+
+    @Then("the field {string} should be:")
+    public void value_of_should_be(String field, String expression) {
+        JavaExecutor.executor().main().returnExpression(field);
+        expectRun(JavaExecutor.executor().main()::evaluate).should(expression);
     }
 }
--- jfactory/src/test/java/com/github/leeonky/jfactory/cucumber/Steps.java
+++ jfactory/src/test/java/com/github/leeonky/jfactory/cucumber/Steps.java
@@ -3,26 +3,12 @@ package com.github.leeonky.jfactory.cucumber;
 import com.github.leeonky.util.JavaExecutor;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-
-import static com.github.leeonky.dal.Assertions.expectRun;
 
 public class Steps {
 
     @Before
     public void importDependencies() {
         JavaExecutor.executor().main().importDependency("com.github.leeonky.jfactory.*");
-    }
-
-    @Then("the result should be:")
-    public void the_result_should_be(String expression) {
-        expectRun(JavaExecutor.executor().main()::evaluate).should(expression);
-    }
-
-    @Then("the field {string} should be:")
-    public void value_of_should_be(String field, String expression) {
-        JavaExecutor.executor().main().returnExpression(field);
-        expectRun(JavaExecutor.executor().main()::evaluate).should(expression);
     }
 
     @Given("the following spec definition:")
