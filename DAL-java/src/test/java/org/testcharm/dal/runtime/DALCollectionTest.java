1ff12a18bdae1087c5ecb681472fe927b8196506
leeonky <leeonky@gmail.com>
2026-06-23 16:21:51 +0000

    Add test

--- DAL-java/src/test/java/org/testcharm/dal/runtime/DALCollectionTest.java
+++ DAL-java/src/test/java/org/testcharm/dal/runtime/DALCollectionTest.java
@@ -1,14 +1,18 @@
 package org.testcharm.dal.runtime;
 
-import org.testcharm.dal.extensions.MapPropertyAccessor;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.testcharm.dal.extensions.MapPropertyAccessor;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
-import static org.testcharm.dal.Assertions.expect;
 import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.testcharm.dal.Assertions.expect;
 
 class DALCollectionTest {
 
@@ -120,6 +124,141 @@ class DALCollectionTest {
 
                 expect(collection.filter(i -> (int) i == 2)).should(": { values= [2] firstIndex: 1}");
             }
+        }
+    }
+
+    @Nested
+    class Decorated {
+        private final DALCollection<String> collection = mock(DALCollection.class);
+        private final DALCollection.Decorated<String> decorated = new DALCollection.Decorated<>(collection);
+
+        @Test
+        void size() {
+            decorated.size();
+
+            verify(collection).size();
+        }
+
+        @Test
+        void getByIndex() {
+            when(collection.getByIndex(100)).thenReturn("test");
+
+            assertEquals("test", decorated.getByIndex(100));
+
+            verify(collection).getByIndex(100);
+        }
+
+        @Test
+        void iterator() {
+            decorated.iterator();
+
+            verify(collection).iterator();
+        }
+
+        @Test
+        void firstIndex() {
+            when(collection.firstIndex()).thenReturn(5);
+
+            assertEquals(5, decorated.firstIndex());
+        }
+
+        @Test
+        void collect() {
+            when(collection.collect()).thenReturn(asList("a", "b", "c"));
+
+            assertEquals(asList("a", "b", "c"), decorated.collect());
+        }
+
+        @Test
+        void filter() {
+            DALCollection filter = mock(DALCollection.class);
+            when(collection.filter(any())).thenReturn(filter);
+
+            Predicate p = mock(Predicate.class);
+
+            assertEquals(filter, decorated.filter(p));
+
+            verify(collection).filter(p);
+        }
+
+        @Test
+        void infinite() {
+            when(collection.infinite()).thenReturn(true);
+
+            assertEquals(true, decorated.infinite());
+        }
+
+        @Test
+        void not_infinite() {
+            when(collection.infinite()).thenReturn(false);
+
+            assertEquals(false, decorated.infinite());
+        }
+
+        @Test
+        void limit() {
+            DALCollection limit = mock(DALCollection.class);
+            when(collection.limit(10)).thenReturn(limit);
+
+            assertEquals(limit, decorated.limit(10));
+
+            verify(collection).limit(10);
+        }
+
+        @Test
+        void values() {
+            Stream<String> stream = mock(Stream.class);
+            when(collection.values()).thenReturn(stream);
+
+            assertEquals(stream, decorated.values());
+        }
+
+        @Test
+        void indexes() {
+            Stream<Integer> stream = mock(Stream.class);
+            when(collection.indexes()).thenReturn(stream);
+
+            assertEquals(stream, decorated.indexes());
+        }
+
+        @Test
+        void map() {
+            DALCollection map = mock(DALCollection.class);
+            IndexedElement.Mapper<String, String> mapper = mock(IndexedElement.Mapper.class);
+            when(collection.map(mapper)).thenReturn(map);
+
+            assertEquals(map, decorated.map(mapper));
+
+            verify(collection).map(mapper);
+        }
+
+        @Test
+        void stream() {
+            Stream<IndexedElement<String>> stream = mock(Stream.class);
+            when(collection.stream()).thenReturn(stream);
+
+            assertEquals(stream, decorated.stream());
+        }
+
+        @Test
+        void requireLimitedCollection() {
+            assertEquals(decorated, decorated.requireLimitedCollection("message"));
+
+            verify(collection).requireLimitedCollection("message");
+        }
+
+        @Test
+        void is_empty() {
+            when(collection.isEmpty()).thenReturn(true);
+
+            assertEquals(true, decorated.isEmpty());
+        }
+
+        @Test
+        void not_empty() {
+            when(collection.isEmpty()).thenReturn(false);
+
+            assertEquals(false, decorated.isEmpty());
         }
     }
 }
